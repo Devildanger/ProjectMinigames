71225e7da3bdff63f99fc37ddde77f6657cc259d
Devildanger <34213701+Devildanger@users.noreply.github.com>
2017-12-04 09:44:49 +0100

    Update minigame3.py

--- minigame3.py
+++ minigame3.py
@@ -4,11 +4,60 @@
 
 #Libraries importeren
 import pygame
-import globals
+import sys
+pygame.init()
+from pygame.locals import *
+
+White = (255,255,255)
+Black = (0,0,0)
+#clock = pygame.time.clock()
 
 
-#Globale variabelen
+gameDisplay = pygame.display.set_mode ((1032,860))
+pygame.display.set_caption("Dineerbeer")
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, image_file, location):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+
+class Berry(pygame.sprite.Sprite):
+    def __init__(self, image_file, location):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
 
 
-#Minigame
+#pygame.draw.rect()
+gameExit = False
+Background = Background("Background.JPG", (0,0))
+Berry = Berry("berry.jpg", (480,230))
+
+
+
+
+    
+while not gameExit:
+     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+           gameExit = True
+     gameDisplay.fill([255, 255, 255])
+     gameDisplay.blit(Background.image, Background.rect)
+     gameDisplay.blit(Berry.image, Berry.rect)
+     pygame.display.update()
+
+
+
+
+
+
+
+
+
+
+pygame.quit()
+quit()
 
