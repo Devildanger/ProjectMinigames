6915ca6f6e8b36ade13f7fa8230d01f355e6f1a5
Loek van den Berg <0941224@hr.nl>
2017-12-07 14:49:39 +0100

    Berry starting

--- minigame3.py
+++ minigame3.py
@@ -2,68 +2,94 @@
 #Minigame 3: Bessen
 #AUTEUR: Loek v/d Berg
 
+
+
+
+
+
 #Libraries importeren
-import pygame
-import os, sys
+import pygame, os, sys
 from pygame.locals import *
 pygame.init()
 
 #Globale waarden
-screenWidth = 1024
-screenHeight = 768
-
-
-screen = pygame.display.set_mode ((screenWidth,screenHeight))
+screenWidth     = 1024
+screenHeight    = 768
+screen          = pygame.display.set_mode ((screenWidth,screenHeight))
+clock           = pygame.time.Clock()
 pygame.display.set_caption("Bessenbeer")
-clock = pygame.time.Clock()
 
-BG = pygame.image.load ("data/backgrounds/Minigame 3 Loek/Background3.png")
-BearIMG = pygame.image.load("data/sprites/Minigame 3 Loek/bearright.jpg")
+
+#Images
+berry           = pygame.image.load("data/sprites/Minigame 3 Loek/berry.jpg")
+BG              = pygame.image.load("data/backgrounds/Minigame 3 Loek/Background3.png")
+BearIMG         = pygame.image.load("data/sprites/Minigame 3 Loek/bearright.jpg")
+
+
+
+
+
+
+
 
 def Bear (x,y):
     screen.blit(BearIMG,(x,y))
 
-bearx = 90
-beary = 370
+def spawn_berries():
+    screen.blit(berry,(220,370))
+    screen.blit(berry,(220,300))
+    screen.blit(berry,(220,240))
+    screen.blit(berry,(220,185))
+    screen.blit(berry,(220,55))
+    screen.blit(berry,(220,420))
+    screen.blit(berry,(220,470))
+    screen.blit(berry,(220,510))
+    screen.blit(berry,(220,565))
+    screen.blit(berry,(220,620))
+    screen.blit(berry,(220,690))
 
-bearx_change = 0
-beary_change = 0
+def Gameloop():
+    bearx = 90
+    beary = 370
 
-Running = True
-while Running:
-    screen.blit (BG,(0,0))
-    for event in pygame.event.get():
-       if event.type == pygame.QUIT:
-          Running = False
-       if event.type == pygame.KEYDOWN:
-           if event.key == pygame.K_LEFT:
-               bearx_change = -5           
-           elif event.key == pygame.K_RIGHT:
-               bearx_change = 5
-           elif event.key == pygame.K_UP:
-               beary_change = -5
-           elif event.key == pygame.K_DOWN:
-               beary_change = 5
-       if event.type == pygame.KEYUP:
-           if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-              bearx_change = 0
-              beary_change = 0
+    bearx_change = 0
+    beary_change = 0
+    Running = True
+    while Running:
+            screen.blit (BG,(0,0))
+            spawn_berries()
+            for event in pygame.event.get():
+               if event.type == pygame.QUIT:
+                  Running = False
+               if event.type == pygame.KEYDOWN:
+                   if event.key == pygame.K_LEFT:
+                       bearx_change = -5           
+                   elif event.key == pygame.K_RIGHT:
+                       bearx_change = 5
+                   elif event.key == pygame.K_UP:
+                       beary_change = -5
+                   elif event.key == pygame.K_DOWN:
+                       beary_change = 5
+               if event.type == pygame.KEYUP:
+                   if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                      bearx_change = 0
+                      beary_change = 0
 
-    bearx += bearx_change
-    beary += beary_change
-    
-    Bear(bearx,beary)
-    pygame.display.update()
-    clock.tick(60)
-
-
-
-
-
-
+            bearx += bearx_change
+            beary += beary_change
+            
+            Bear(bearx,beary)
+            pygame.display.update()
+            clock.tick(60)
 
 
 
+
+
+
+
+
+Gameloop()
 pygame.quit()
 quit()
 
