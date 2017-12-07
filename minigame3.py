59bf9680dd7506fe5ec6db79f2a8c08c36c3d2ff
Loek van den Berg <0941224@hr.nl>
2017-12-07 13:54:45 +0100

    Bear Walking

--- minigame3.py
+++ minigame3.py
@@ -15,6 +15,7 @@ screenHeight = 768
 
 screen = pygame.display.set_mode ((screenWidth,screenHeight))
 pygame.display.set_caption("Bessenbeer")
+clock = pygame.time.Clock()
 
 BG = pygame.image.load ("data/backgrounds/Minigame 3 Loek/Background3.png")
 BearIMG = pygame.image.load("data/sprites/Minigame 3 Loek/bearright.jpg")
@@ -25,15 +26,35 @@ def Bear (x,y):
 bearx = 90
 beary = 370
 
+bearx_change = 0
+beary_change = 0
+
 Running = True
 while Running:
     screen.blit (BG,(0,0))
     for event in pygame.event.get():
        if event.type == pygame.QUIT:
           Running = False
+       if event.type == pygame.KEYDOWN:
+           if event.key == pygame.K_LEFT:
+               bearx_change = -5           
+           elif event.key == pygame.K_RIGHT:
+               bearx_change = 5
+           elif event.key == pygame.K_UP:
+               beary_change = -5
+           elif event.key == pygame.K_DOWN:
+               beary_change = 5
+       if event.type == pygame.KEYUP:
+           if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+              bearx_change = 0
+              beary_change = 0
+
+    bearx += bearx_change
+    beary += beary_change
+    
     Bear(bearx,beary)
     pygame.display.update()
-
+    clock.tick(60)
 
 
 
